1676abfd7f915784c3de6cad9ef3cd6eb719dbf4
luminary <luminaryhero80@gmail.com>
2023-12-21 16:13:39 +0800

    deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,10 +13,10 @@ const config = {
   favicon: "img/favicon.ico",
 
   // Set the production url of your site here
-  url: "https://luminaryhero.github.io",
+  url: "https://luminaryhero.github.io/Nest-wiki",
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: "/Nest-wiki/",
+  baseUrl: "/",
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
