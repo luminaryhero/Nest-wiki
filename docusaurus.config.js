025a23c08d7e04163c095ed0df65520d22b57e30
luminary <luminaryhero80@gmail.com>
2023-12-25 22:15:53 +0800

    update: blog

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -68,7 +68,7 @@ const config = {
             position: "left",
             label: "Docs",
           },
-          // { to: "/blog", label: "Blog", position: "left" },
+          { to: "/blog", label: "Blog", position: "left" },
           {
             href: "https://github.com/luminaryhero/nest-wiki",
             label: "GitHub",
